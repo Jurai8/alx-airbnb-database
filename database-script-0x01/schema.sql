56d0c147160be1fbd517ee12d1f3ae390e53b923
Charles <131194505+Jurai8@users.noreply.github.com>
2025-06-29 18:58:22 +0200

    schema

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,60 +1,73 @@
 
 CREATE TABLE User (
-    user_id UUID PRIMARY KEY,
-    first_name VARCHAR(255), NOT NULL
-    last_name VARCHAR(255), NOT NULL
-    email VARCHAR(255), UNIQUE, NOT NULL
-    password_hash VARCHAR(255), NOT NULL
-    phone_number VARCHAR(20), NULL
-    role ENUM ('guest', 'host', 'admin'), NOT NULL
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    user_id VARCHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NULL,
+    role ENUM ('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 CREATE TABLE Property (
-    property_id UUID PRIMARY KEY,  
-    host_id FOREIGN KEY, REFERENCES User(user_id)
-    name VARCHAR, NOT NULL
-    description TEXT, NOT NULL
-    location VARCHAR, NOT NULL
-    pricepernight DECIMAL, NOT NULL
-    created_at TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
-    updated_at TIMESTAMP, ON UPDATE CURRENT_TIMESTAMP
+    property_id VARCHAR(36) PRIMARY KEY,  
+    host_id VARCHAR(36) NOT NULL,
+    name VARCHAR NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR NOT NULL,
+    pricepernight DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
+    FOREIGN KEY (host_id) REFERENCES User(user_id)
 )
 
 CREATE TABLE Booking (
-    booking_id UUID PRIMARY KEY, 
-    property_id FOREIGN KEY, REFERENCES Property(property_id)
-    user_id FOREIGN KEY, REFERENCES User(user_id)
-    start_date DATE, NOT NULL
-    end_date DATE, NOT NULL
-    total_price DECIMAL, NOT NULL
-    status ENUM (pending, confirmed, canceled), NOT NULL
-    created_at TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+    booking_id VARCHAR(36) PRIMARY KEY, 
+    property_id VARCHAR(36) NOT NULL, 
+    user_id VARCHAR(36) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status ENUM ('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
 
 )
 
 CREATE TABLE Payment (
-    payment_id UUID PRIMARY KEY, 
-    booking_id FOREIGN KEY, REFERENCES Booking(booking_id)
-    amount DECIMAL, NOT NULL
-    payment_date TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
-    payment_method ENUM (credit_card, paypal, stripe), NOT NULL
+    payment_id VARCHAR(36) PRIMARY KEY,
+    booking_id VARCHAR(36) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM ('credit_card', 'paypal', 'stripe') NOT NULL,
+
+    FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
 )
 
 CREATE TABLE Review (
-    review_id UUID PRIMARY KEY, 
-    property_id FOREIGN KEY, REFERENCES Property(property_id)
-    user_id FOREIGN KEY, REFERENCES User(user_id)
-    rating INTEGER, CHECK rating >= 1 AND rating <= 5, NOT NULL
-    comment TEXT, NOT NULL
-    created_at TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+    review_id VARCHAR(36) PRIMARY KEY, 
+    property_id VARCHAR(36) NOT NULL, 
+    user_id VARCHAR(36) NOT NULL, 
+    rating INTEGER NOT NULL,
+    CHECK (rating >= 1 AND rating <= 5), 
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
 )
 
 CREATE TABLE Message (
-    message_id UUID PRIMARY KEY, 
-    sender_id FOREIGN KEY, REFERENCES User(user_id)
-    recipient_id FOREIGN KEY, REFERENCES User(user_id)
-    message_body TEXT, NOT NULL
-    sent_at TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+    message_id VARCHAR(36) PRIMARY KEY,
+    sender_id VARCHAR(36) NOT NULL,
+    recipient_id VARCHAR(36) NOT NULL,
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (sender_id) REFERENCES User(user_id),
+    FOREIGN KEY (recipient_id) REFERENCES User(user_id)
 )
