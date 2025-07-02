7784cd0235aa74565129c6eb64bccfa7d2b51000
Charles <131194505+Jurai8@users.noreply.github.com>
2025-07-02 20:09:40 +0200

    joins sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,4 @@
+SELECT name, start_date, end_date, first_name, last_name
+FROM User
+INNER JOIN Booking ON User.user_id = Booking.user_id
+INNER JOIN Property ON Booking.property_id = Property.property_id
