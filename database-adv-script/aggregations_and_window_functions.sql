9a0621dc47c8de632b59ea828385cbb5efd5c2ef
Charles <131194505+Jurai8@users.noreply.github.com>
2025-07-12 08:38:51 +0200

    window functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,23 @@
+
+/* total number of bookings made by each user */
+SELECT user_id
+FROM booking 
+GROUP BY user_id 
+HAVING COUNT(user_id) >= 3;
+
+/* rank properties based on the total number of bookings */
+WITH PropertyBookingCounts AS (
+    SELECT
+        property_id,
+        COUNT(booking_id) AS total_bookings
+    FROM
+        booking
+    GROUP BY
+        property_id
+)
+SELECT
+    property_id,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS property_rank
+FROM
+    PropertyBookingCounts;
